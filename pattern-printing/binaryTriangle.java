c6a75c6c8eb545b742b7d96f2acd09117a1f1f02
Karthik Shettigar <86278623+Karthikkk-24@users.noreply.github.com>
2024-10-27 10:33:56 +0530

    Pattern Printing Code Added 3

---
+++ pattern-printing/binaryTriangle.java
@@ -0,0 +1,17 @@
+public class binaryTriangle {
+    public static void main(String[] args) {
+        int n = 5;
+        int counter = 1;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                if ((i + j) % 2 != 0) {
+                    System.out.print("0 ");
+                } else {
+                    System.out.print("1 ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+}
