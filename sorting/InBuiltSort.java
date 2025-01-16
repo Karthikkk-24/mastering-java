03ea5c06b788fe51c22c735ff8b8c4b96c729996
Karthik Shettigar <86278623+Karthikkk-24@users.noreply.github.com>
2025-01-16 23:35:17 +0530

    inbultsort code added

--- sorting/InBuiltSort.java
+++ sorting/InBuiltSort.java
@@ -4,9 +4,15 @@ import java.util.Arrays;
 public class InBuiltSort {
     public static void main(String[] args) {
         int[] arr = {64, 34, 25, 12, 22, 11, 90};
+        int[] arr2 = {64, 34, 25, 12, 22, 11, 90};
         java.util.Arrays.sort(arr);
         for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+        java.util.Arrays.sort(arr2, 0, 3);
+        for (int i = 0; i < arr2.length; i++) {
+            System.out.print(arr2[i] + " ");
         }
     }
 }
